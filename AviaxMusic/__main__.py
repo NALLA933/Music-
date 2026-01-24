604702938d002511da1ac006d16db08d08fceb03
Piyush <piyush960yogi@gmail.com>
2026-01-25 01:21:54 +0530

    Update __main__.py

--- AviaxMusic/__main__.py
+++ AviaxMusic/__main__.py
@@ -2,7 +2,6 @@ import asyncio
 import importlib
 
 from pyrogram import idle
-from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
 from AviaxMusic import LOGGER, app, userbot
@@ -41,11 +40,6 @@ async def init():
     await Aviax.start()
     try:
         await Aviax.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
-    except NoActiveGroupCall:
-        LOGGER("AviaxMusic").error(
-            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
-        )
-        exit()
     except:
         pass
     await Aviax.decorators()
