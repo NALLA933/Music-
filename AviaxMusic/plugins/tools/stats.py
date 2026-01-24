788f3fbe0c2bd4c4092012c4ade87ab7119a0675
Piyush <piyush960yogi@gmail.com>
2026-01-25 01:23:05 +0530

    Update stats.py

--- AviaxMusic/plugins/tools/stats.py
+++ AviaxMusic/plugins/tools/stats.py
@@ -2,11 +2,11 @@ import platform
 from sys import version as pyver
 
 import psutil
+import pytgcalls
 from pyrogram import __version__ as pyrover
 from pyrogram import filters
 from pyrogram.errors import MessageIdInvalid
 from pyrogram.types import InputMediaPhoto, Message
-from pytgcalls.__version__ import __version__ as pytgver
 
 import config
 from AviaxMusic import app
@@ -17,6 +17,8 @@ from AviaxMusic.utils.database import get_served_chats, get_served_users, get_su
 from AviaxMusic.utils.decorators.language import language, languageCB
 from AviaxMusic.utils.inline.stats import back_stats_buttons, stats_buttons
 from config import BANNED_USERS
+
+pytgver = pytgcalls.__version__
 
 
 @app.on_message(filters.command(["stats", "gstats"]) & filters.group & ~BANNED_USERS)
